3e9ce9e2c0fe11cdacfaaf82844069214ea663ea
tomliy <tomliy@github.com>
2026-06-10 17:48:49 +0800

    Fix PCIe port mapping: move 2.4G from pcie0 to pcie2

--- scripts/fix-dts.py
+++ scripts/fix-dts.py
@@ -5,16 +5,43 @@ target = sys.argv[1]
 with open(target) as f:
     content = f.read()
 
-# Fix pcie1 first: remove freq-limit, change offset to 0x0000
-old = "\t\tmediatek,mtd-eeprom = <&factory 0x8000>;\n\t\tieee80211-freq-limit = <5000000 6000000>;"
-new = "\t\tmediatek,mtd-eeprom = <&factory 0x0000>;"
-content = content.replace(old, new, 1)
+# Step 1: Remove pcie0 wifi node entirely
+# Physical port 0 has no card, the 2.4G chip is on pcie2
+pcie0_block = (
+    '&pcie0 {\n'
+    '\twifi0: wifi@0,0 {\n'
+    '\t\tcompatible = \"pci14c3,7603\";\n'
+    '\t\treg = <0x0000 0 0 0 0>;\n'
+    '\t\tmediatek,mtd-eeprom = <&factory 0x0000>;\n'
+    '\t};\n'
+    '};\n'
+)
 
-# Fix pcie0: change offset to 0x8000, add freq-limit
-old = "\t\tmediatek,mtd-eeprom = <&factory 0x0000>;\n\t};\n};\n\n&pcie1"
-new = "\t\tmediatek,mtd-eeprom = <&factory 0x8000>;\n\t\tieee80211-freq-limit = <5000000 6000000>;\n\t};\n};\n\n&pcie1"
-content = content.replace(old, new, 1)
+if pcie0_block in content:
+    content = content.replace(pcie0_block, '', 1)
+    print('Removed pcie0 wifi node (port 0 has no card)')
+else:
+    print('WARNING: pcie0 wifi block not found!')
 
-with open(target, "w") as f:
+# Step 2: Add pcie2 wifi node for the 2.4G chip (MT7603EN on physical port 2)
+wifi2_block = (
+    '&pcie2 {\n'
+    '\twifi0: wifi@0,0 {\n'
+    '\t\tcompatible = \"pci14c3,7603\";\n'
+    '\t\treg = <0x0000 0 0 0 0>;\n'
+    '\t\tmediatek,mtd-eeprom = <&factory 0x0000>;\n'
+    '\t};\n'
+    '};\n'
+)
+
+# Insert after pcie1 block (before &ethernet, NOT inside it)
+marker = '&ethernet'
+if marker in content:
+    content = content.replace(marker, wifi2_block + '\n' + marker, 1)
+    print('Added pcie2 wifi node for 2.4G chip')
+else:
+    print('WARNING: ethernet marker not found!')
+
+with open(target, 'w') as f:
     f.write(content)
-print("DTS fixed: " + target)
+print('DTS fixed: ' + target)
